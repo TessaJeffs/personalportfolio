4b97cbc708989aaac885817165d1bec6d6e2cfb4
Tessa Jeffs <just.tes17@gmail.com>
2020-03-10 21:04:45 -0600

    why is it still broken

--- starwars/people/starwars.js
+++ starwars/people/starwars.js
@@ -4,8 +4,10 @@ import {people} from '../../data/people.js'
 let gallery = document.querySelector ('.gallery')
 
 const maleCharacters = people.filter(person => person.gender === 'male')
+
 const femaleCharacters = people.filter (person => person.gender === 'female')
-const otherCharacters = people.filter (
+
+const otherCharacters = people.filter(
     person => 
     person.gender === 'n/a'||
     person.gender === 'none' ||
@@ -19,44 +21,54 @@ let otherButton = document.querySelector ('#otherButton')
 maleButton.addEventListener ('click', function(event){
     populateDOM(maleCharacters)
 })
+
 femaleButton.addEventListener ('click', function(event){
     populateDOM(femaleCharacters)
 })
+
 otherButton.addEventListener ('click', function(event){
     populateDOM(otherCharacters)
 })
 
 function getLastNumber(url) {
     let end =  url.lastIndexOf('/')
-    let start = end -2 
+    let start = end - 2 
     if (url.charAt(start) === "/"){
         start++
     }
     return url.slice(start,end)
 }
-function removeChildren(element){
-    while (element.firstChild) {element.removeChild(element.firstChild)
+
+function removeChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
     }
 }
+
 function populateDom(characters) {
-removeChildren(gallery) 
-characters.forEach(person => {
+  removeChildren(gallery) 
+  characters.forEach(person => {
     let imageNum = getLastNumber(person.url)
     let personAnchor = document.createElement('a')
     personAnchor.href = '#'
     let personImg = document.createElement ('img')
-    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
-})
-    }
-}
-        personImg.addEventListener ('error', event => {
-            personImg.hidden = true
-        })
-personImg.addEventListener("click", function (event){
+    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${imageNum}.jpg`
+
+ personImg.addEventListener ('error', event => {
+     personImg.hidden = true
+
+    }) 
+
+     personImg.addEventListener("click", function (event){
     console.log ("thanks for clicking!")
-})
-personAnchor.appendChild (personImg)
-galleryDiv.appendChild(personAnchor)
-counter++
     })
-    }
+
+ personAnchor.appendChild (personImg)
+ gallery.appendChild(personAnchor)
+ })
+}
+
+populateDOM(people)
+
+   
+   
